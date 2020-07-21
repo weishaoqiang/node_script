1c5cb2c46fe5acea89a7d4f706700db88315da40
weishaoqiang <1430558779@qq.com>
2020-07-21 23:28:27 +0800

    gh-pages update

--- bin/commit.js
+++ bin/commit.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 var shell = require("shelljs")
-shell.exec("git init && git add -A . && git commit -a -m 'gh-pages update'")
+shell.exec("git init && git add -A . && git commit -a -m 'gh-pages update' && git log --oneline")
 // console.log("console.log output")
